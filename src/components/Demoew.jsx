642c5e4149cf218495fdf32e1dfb557d3a8292f9
Raginaakhter <raginaakhter052@gmail.com>
2026-01-04 21:40:01 +0600

    Demoew

---
+++ src/components/Demoew.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Demoew = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste eum dolores laudantium doloribus, incidunt modi 
+      porro dolore ducimus qui odit dicta odio nemo esse nisi sint, officiis impedit est id.
+    </div>
+  );
+};
+
+export default Demoew;
