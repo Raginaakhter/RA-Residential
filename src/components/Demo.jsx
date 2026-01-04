80659e62a8e698e7410e70ea82bfc88733f04aec
Raginaakhter <raginaakhter052@gmail.com>
2026-01-04 21:31:42 +0600

    added

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -5,7 +5,7 @@ const Demo = () => {
         <div>
             
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi non sint architecto, cumque impedit magnam nostrum officia quidem
-           libero rem nam voluptate quos consectetur iste neque incidunt voluptatem autem amet.
+        
         </div>
     );
 };
