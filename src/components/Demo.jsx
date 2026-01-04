15ac37d1cbc1e9561a6f39d4a2438f608903af8f
Raginaakhter <raginaakhter052@gmail.com>
2026-01-04 21:31:13 +0600

    updated demo file

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 
 const Demo = () => {
     return (
-        <div> repudiandae repellat \mmmmmmmmmmmmmmm
+        <div>
             
-          
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi non sint architecto, cumque impedit magnam nostrum officia quidem
+           libero rem nam voluptate quos consectetur iste neque incidunt voluptatem autem amet.
         </div>
     );
 };
