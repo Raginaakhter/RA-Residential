ae0416552ddf0075017b99ebe1ae6a90c343f807
Raginaakhter <raginaakhter052@gmail.com>
2026-01-04 21:35:35 +0600

    adding updated

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -7,8 +7,8 @@ const Demo = () => {
           Lorem ipsum  Excepturi non sint architecto, Lorem ipsum dolor sit
            amet consectetur adipisicing elit. Obcaecati nemo quidem maxime est
             minima perspiciatis accusamus quasi in dolorum. Accusantium nostrum alias
-           reprehenderit consequuntur tempore unde vitae, eos quasi obcaecati.
         
+    
         </div>
     );
 };
